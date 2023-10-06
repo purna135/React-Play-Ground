44ae2a7707dad326a0e3ce303b2c623b9bc032d3
Purna Chandra Mansingh <purnachandramansingh135@gmail.com>
2023-10-06 12:23:55 +0530

    initial commit

---
+++ Expense-Tracker/src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import "./ExpenseForm.css";
+
+const ExpenseForm = (props) => {
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState("");
+  const [date, setDate] = useState("");
+
+  const titleChangeHandler = (event) => {
+    setTitle(event.target.value);
+  };
+  const amountChangeHandler = (event) => {
+    setAmount(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    setDate(event.target.value);
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: title,
+      amount: +amount,
+      date: new Date(date),
+    };
+    props.onSaveExpenseData(expenseData);
+    setTitle("");
+    setAmount("");
+    setDate("");
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" value={title} onChange={titleChangeHandler} />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input type="number" value={amount} onChange={amountChangeHandler} />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input type="Date" value={date} onChange={dateChangeHandler} />
+        </div>
+        <div className="new-expense__actions">
+          <button type="submit">
+            Add Expense
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
---
+++ Investment Calculator/src/components/ResultsTable/ResultsTable.js
@@ -0,0 +1,42 @@
+import classes from "./ResultsTable.module.css";
+
+const ResultsTable = (props) => {
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+  return (
+    <table className={classes["result"]}>
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Total Savings</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.results.map((data) => (
+          <tr key={data.year}>
+            <td>{data.year}</td>
+            <td>{formatter.format(data.savingsEndOfYear)}</td>
+            <td>{formatter.format(data.yearlyInterest)}</td>
+            <td>
+              {formatter.format(
+                data.savingsEndOfYear -
+                  props.initialInvestment -
+                  data.yearlyContribution * data.year
+              )}
+            </td>
+            <td>{formatter.format(data.yearlyContribution * data.year)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default ResultsTable;
